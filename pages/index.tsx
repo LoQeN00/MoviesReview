b8e382bf293f6b8b0c1e6abf62ee763d9fbf1c73
LoQeN00 <LoQeN00@o2.pl>
2022-01-21 04:07:17 +0100

    Adding logging with fb

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,12 @@ const Home: NextPage = () => {
 
   },[setMovies])
 
-  console.log(session)
+  
 
 
   return (
     <>
-    <h1>{session ? `Witaj ${session.user?.name}` : `Witaj, aby móc w pełni używać naszego serwisu zaloguj się ${<Link href='/login'><a>Kliknij aby zalogować</a></Link>}`}</h1>
+    {status === "loading" ? <h1>{session ? `Witaj ${session.user?.name}` : `Witaj, aby móc w pełni używać naszego serwisu zaloguj się ${<Link href='/login'><a>Kliknij aby zalogować</a></Link>}`}</h1> : null}
     <div className='h-screen flex justify-center items-center'>
       <div className='flex justify-center items-center'>
         {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
