337044c770b346063567b7db14c792bd293bb2c6
LoQeN00 <LoQeN00@o2.pl>
2022-01-22 19:28:29 +0100

    Adding css to index page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
 import React,{FC} from 'react';
-import { useSession,signOut,signIn } from 'next-auth/react'
+import { useSession,signOut,signIn, getSession, SessionContextValue } from 'next-auth/react'
 import Link from 'next/link';
+import { GetServerSideProps, } from "next"
+import Image from 'next/image';
 
 
-const Header = () => {
 
-    const { data: session, status } = useSession()
+const Header : FC = () => {
+
+  const { data: session, status } = useSession()
+
 
   return (
     <header className='bg-[#2EC4B6] p-8 text-primary text-2xl flex justify-between items-center'>
@@ -13,12 +17,17 @@ const Header = () => {
           <h1 className='text-primary font-bold text-4xl'>MOVIES REVIEW</h1>
         </div>
         <div>
-            {session ?
+
+            {session && session.user ?
                 (
-                  <>
-                    <h1>Witaj {session.user?.name}</h1>
+                  <div>
+                    <div>
+                      <h1>{session.user && session.user.name}</h1>
+                      <Image src={ session.user.image } alt={ session.user.name } />
+                    </div>
+                    
                     <button onClick={() => signOut()}>Wyloguj</button>
-                  </> 
+                  </div> 
                 ):
         
                 <button className='bg-primary px-6 py-4 text-accent rounded-2xl' onClick={() => signIn('facebook', {callbackUrl:"/"})}>Zaloguj się</button>
