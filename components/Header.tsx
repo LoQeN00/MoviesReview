4c4150235c0b241e5f310fe29a603fe6cae0acc4
LoQeN00 <LoQeN00@o2.pl>
2022-01-22 19:49:16 +0100

    Adding css to index page

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,12 +20,11 @@ const Header : FC = () => {
             {session && session.user && session.user.image && session.user.name ?
                 (
                   <div>
-                    <div>
+                    <div className='flex space-x-1'>
                       <h1>{session.user && session.user.name}</h1>
-                      <div className='w-16 h-16 relative'>
+                      <div className='w-16 h-16 relative rounded-full'>
                         <Image layout='fill' src={session.user.image} alt={ session.user.name } />
                       </div>
-                      
                     </div>
                     
                     <button onClick={() => signOut()}>Wyloguj</button>
