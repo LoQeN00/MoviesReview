a6baf9be6c9e6bd73fc7280999ade7fa0df4efdb
LoQeN00 <LoQeN00@o2.pl>
2022-01-27 17:24:01 +0100

    Reaction security

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -78,12 +78,12 @@ const CommentComponent: FC<CommentProps> = ({comment}) => {
                 <div className='bg-secondary p-4 rounded-2xl max-w-[350px] break-words relative'>
                     <p className='font-bold text-md tracking-wide'>{comment.author}</p>
                     <p>{comment.text}</p>
-                    {plusCount && minusCount && (minusCount > 0 || plusCount > 0) ? (
+                    {plusCount && minusCount && (plusCount > 0 || minusCount > 0) ? (
                         <div className='absolute bottom-[-10px] right-[-20px] bg-accent p-1 rounded-lg flex'>
                             <p>{plusCount}+ </p>
                             <p>{minusCount}- </p>
                         </div>
-                        ): null
+                        ): <h1>Chuj</h1>
                         
                     }
                     
