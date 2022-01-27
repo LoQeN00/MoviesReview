c49d131d64377765de7ac4bd5d1289946b468735
LoQeN00 <LoQeN00@o2.pl>
2022-01-27 21:26:58 +0100

    Refactoring React

--- components/CommentsContaier.tsx
+++ components/CommentsContaier.tsx
@@ -1,61 +1,17 @@
-import React, {FC, useRef} from 'react';
+import React, {FC} from 'react';
 import CommentComponent from "../components/Comment"
 import { Comment } from "@prisma/client"
 import { useSession} from 'next-auth/react'
-import {useRouter} from "next/router"
-import {useRecoilState} from "recoil"
+import {useRecoilValue} from "recoil"
 import { commentsState } from "../atoms/commentsAtoms"
 import AddCommentInput from './AddCommentInput';
 
 
 const CommentsContaier: FC = () => {
 
-  const router = useRouter()
-
   const { data: session, status } = useSession()
 
-  const [comments,setComments] = useRecoilState<Comment[] | null>(commentsState)
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
-
-  const addComment = async () => {
-
-    const slug = router.query.slug
-
-    if (inputRef.current) {
-
-        if (inputRef.current.value === "") return
-
-        if (session) {
-
-            if (session.user) {
-
-                const data = await fetch("/api/comments/addComment", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        text: inputRef.current.value,
-                        author: session.user.name,
-                        authorImg: session.user.image,
-                        userId: session.user.userId,
-                        slug
-                    })
-                })
-
-                const updatedDataJson = await fetch(`https://movies-review-three.vercel.app/api/movie/${slug}`)
-
-                // const updatedDataJson = await fetch(`http://localhost:3000/api/movie/${slug}`)
-
-                const updatedData = await updatedDataJson.json()
-    
-                setComments(updatedData.comments)
-    
-                inputRef.current.value = ""
-
-                }
-            }
-        }
-    }
+  const comments = useRecoilValue<Comment[] | null>(commentsState)
 
   if(!comments) return null
 
