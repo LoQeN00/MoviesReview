8c88198bdf0a324300b56b9072cb1e79d5dcf17d
LoQeN00 <LoQeN00@o2.pl>
2022-01-27 14:33:54 +0100

    Removing reactions

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -13,19 +13,6 @@ const CommentComponent: FC<CommentProps> = ({comment}) => {
     const { data: session, status } = useSession()
 
 
-    const addReaction = async (reaction: "+" | "-") => {  
-        // const data = fetch('https://movies-review-three.vercel.app/api/comments/addReaction')
-        const data = await fetch("/api/comments/addReaction", {
-            method: "POST",
-            body: JSON.stringify({
-                commentId: comment.id,
-                reactionType: reaction
-            })
-        })
-
-    }
-
-
     return (
         <div className='text-white max-w-lg flex p-4'>
             <div className='w-[64px] h-[64px] relative mr-5'>
@@ -35,21 +22,9 @@ const CommentComponent: FC<CommentProps> = ({comment}) => {
                 <div className='bg-secondary p-4 rounded-2xl max-w-[350px] break-words relative'>
                     <p className='font-bold text-md tracking-wide'>{comment.author}</p>
                     <p>{comment.text}</p>
-                    <div className='absolute bottom-[-18px] right-[-10px] w-14 h-8 bg-accent rounded-lg flex justify-center items-center'>
-                        <p><span>{comment.plus}+</span>  <span>{comment.minus}-</span></p>
-                    </div>
+                    
                 </div>
-                {session && <div className='space-x-4 mt-2 flex'>
-                    {/* <div onClick={() => addReaction("+")} className='border-2 border-secondary rounded-full w-7 cursor-pointer'>
-                        <p className='text-center'>+</p>
-                    </div>
-                    <div onClick={() => addReaction("-")} className='border-2 border-secondary rounded-full w-7 cursor-pointer'>
-                        <p className='text-center'> - </p>
-                    </div> */}
-                </div>}
             </div>
-           
-            
         </div>
     )
 }
